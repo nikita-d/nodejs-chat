df9de7aa8151a9064becbadd1d4d2a5a944d0d80
Nikita Dubrovin <nikita.dubrovin@gmail.com>
2016-04-18 19:26:17 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
-module.exports.getConfig = function(type) {
+'use strict';
 
-}
-
+module.exports = {
+    context: __dirname + '/app',
+    entry: './index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'main.js'
+    },
+    debug: true,
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    }
+};
